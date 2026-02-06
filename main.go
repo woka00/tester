50e317bfac365508507d9696f22ec22bcb61bd60
woka <firemelt@yandex.ru>
2026-02-07 01:02:41 +0300

    add simple_connection

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"git/feature1"
 	"git/feature2"
+	"git/feature_postgres/simple_connection"
 )
 
 func main() {
 	fmt.Println("Hello, Git!")
 	feature1.Feature1()
 	feature2.Feature2()
+	simple_connection.CheckConnection()
 }
