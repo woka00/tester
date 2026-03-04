0464a405ea45ea090e95c6535640f11616e3b8ca
wokaLINX <firemelt@yandex.ru>
2026-03-04 15:57:43 +0300

    safe sensetivity files

--- feature_postgres/simple_connection/simple_connection.go
+++ feature_postgres/simple_connection/simple_connection.go
@@ -2,11 +2,13 @@ package simple_connection
 
 import (
 	"context"
+	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
 // "postgres://YourUserName:YourPassword@YourHostName:5432/YourDatabaseName"
 func CreateConnection(ctx context.Context) (*pgx.Conn, error) {
-	return pgx.Connect(ctx, "postgres://postgres:admin@localhost:5432/postgres")
+	connString := os.Getenv("CONN_STRING")
+	return pgx.Connect(ctx, connString)
 }
